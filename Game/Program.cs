8d628850cb15bcd6f5b880696dd71d509c2d7f4e
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-08-02 22:17:17 +0200

    Add key handling (+)

--- Game/Program.cs
+++ Game/Program.cs
@@ -12,21 +12,35 @@
             {
                 for (int j = 0; j < board.GetLength(1); j++)
                 {
-                    Console.Write($" {board[i, j]}");
+                    Console.Write($"   {board[i, j]}");
                 }
-                Console.WriteLine();
+                Console.WriteLine("\n");
             }
         }
 
         static void Main()
         {
             int[,] board = new int[4, 4];
+            bool run = true;
 
             Console.Title = "Game 2048";
-            while (true)
+            while (run)
             {
                 UpdateScreen(board);
-                Console.ReadKey();
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.Escape:
+                        run = false;
+                        break;
+                    case ConsoleKey.W:
+                        break;
+                    case ConsoleKey.S:
+                        break;
+                    case ConsoleKey.A:
+                        break;
+                    case ConsoleKey.D:
+                        break;
+                }
             }
         }
     }
