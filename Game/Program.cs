fb699818564237ff12f035edb64a045638e06e27
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-08-02 12:57:45 +0200

    Basic program functionality

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace Game
+{
+    class Program
+    {
+        static void UpdateScreen(int[,] board)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("  Use 'WASD' to move");
+            Console.WriteLine();
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    Console.Write($" {board[i, j]}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main()
+        {
+            int[,] board = new int[4, 4];
+
+            Console.Title = "Game 2048";
+            while (true)
+            {
+                UpdateScreen(board);
+                Console.ReadKey();
+            }
+        }
+    }
+}
