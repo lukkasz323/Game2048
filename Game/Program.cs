cdeadaff19e7415b4242ea15c1e10e9e6ad3937b
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-08-19 01:14:49 +0200

    Improve game ending

--- Game/Program.cs
+++ Game/Program.cs
@@ -17,7 +17,7 @@ namespace Game
         static void UpdateScreen(int[,] board)
         {
             Console.Clear();
-            Console.WriteLine("\n  Use 'WASD' to move\n");
+            Console.WriteLine("\n  Use 'WASD' to move | 'Esc' to quit\n");
             for (int i = 0; i < board.GetLength(0); i++)
             {
                 for (int j = 0; j < board.GetLength(1); j++)
@@ -54,11 +54,11 @@ namespace Game
             } 
         }
 
-        static Input UserInput()
+        static Input GetUserInput()
         {
             ConsoleKeyInfo key;
 
-            key = Console.ReadKey();
+            key = Console.ReadKey(true);
             return key.Key switch
             {
                 ConsoleKey.Escape => Input.Quit,
@@ -85,22 +85,49 @@ namespace Game
             }
         }
 
+        static bool GameHasEnded(Input input, int[,] board)
+        {
+            if (input == Input.Quit)
+            {
+                return true;
+            }
+
+            foreach (int n in board)
+            {
+                if (n == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         static void Main()
         {
             Input input;
             int[,] board = new int[4, 4];
-            
+            bool run = true;
+
             Console.Title = "Game 2048";
             SpawnNumber(board);
-            while (true)
+            while (run)
             {
                 SpawnNumber(board);
                 UpdateScreen(board);
-                input = UserInput();
-                if (input == Input.Quit)
+                while (true)
                 {
-                    break;
+                    input = GetUserInput();
+                    if (GameHasEnded(input, board))
+                    {
+                        run = false;
+                    }
+                    if (input != Input.None)
+                    {
+                        break;
+                    }
                 }
+                
             }
         }
     }
